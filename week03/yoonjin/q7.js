093bef609f1597a764dd9d2822d9415fada909cc
nacho-0 <iridium2827@gmail.com>
2025-04-05 18:57:13 +0900

    homework3 + study

---
+++ week03/yoonjin/q7.js
@@ -0,0 +1,8 @@
+//연습문제 7
+for (let i = 1; i <= 5; i++) {
+  let star = "";
+  for (let j = 0; j < i; j++) {
+    star += "*";
+  }
+  console.log(star);
+}
---
+++ week03/yoonjin/study.js
@@ -0,0 +1,3 @@
+//prompt('숫자 맞추기 게임 시작!\n사용자 입력:')
+let num = Math.floor(Math.random() * 100) + 1;
+console.log(num);
