3c85403a499dc672fc9c8a713b32a507c6d7dcfe
nacho-0 <iridium2827@gmail.com>
2025-05-26 12:58:00 +0900

    week07_최윤진

---
+++ week07/최윤진/todo-list/todo-list/src/App.jsx
@@ -0,0 +1,79 @@
+import { useReducer, useRef } from "react";
+import "./App.css";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+
+// 초기 데이터
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "기깔나게 자기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "푸짐하게 먹기",
+    date: new Date().getTime(),
+  },
+];
+
+// 리듀서 함수 정의
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.newTodo, ...state];
+    case "UPDATE":
+      return state.map((todo) =>
+        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo
+      );
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const idRef = useRef(3); //idRef.current === 3
+  const [todos, dispatch] = useReducer(todoReducer, mockData); //todos 배열 저장.
+  // todoReducer: 상태 변경 로직을 담은 함수/ mockData:초기 상태 값/ todos: 현재 상태/ dispatch: 상태를 업데이트하는 함수
+  // 할 일 추가
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    };
+    dispatch({ type: "CREATE", newTodo });
+  };
+
+  // 완료 체크 토글
+  const onUpdate = (targetId) => {
+    dispatch({ type: "UPDATE", targetId });
+  };
+
+  // 삭제
+  const onDelete = (targetId) => {
+    dispatch({ type: "DELETE", targetId });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+    </div>
+  );
+}
+
+export default App;
