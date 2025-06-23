b7c543346e2db84a9e10cabbbcfed8b343d3ebce
Yuno-Jung <duwn126@gmail.com>
2025-06-23 14:44:53 +0900

    week09_정윤호

---
+++ week09/정윤호/Toyproject/src/App.jsx
@@ -0,0 +1,106 @@
+// 라우팅 & 상태 관리 중심
+
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import Notfound from "./pages/Notfound";
+import { createContext, useReducer, useRef } from "react";
+
+// Context 정의
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
+
+// 테스트용 초기 데이터
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+// reducer 정의
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter(
+        (item) => String(item.id) !== String(action.id) // 🔧 수정됨
+      );
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 일기 생성 함수
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 일기 수정 함수
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 일기 삭제 함수
+  const onDelete = (id) => {
+   
+    dispatch({
+      type: "DELETE",
+      id, 
+    });
+  };
+
+  return (
+    <>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    </>
+  );
+}
+
+export default App;
